98f1d4195e725786d82aa0df364dfef6b66f5975
Polinacherkay <cherkaj.pd@edu.spbstu.ru>
2019-12-16 21:05:34 +0300

    5

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -247,8 +247,10 @@ fun findCheapestStuff(stuff: Map<String, Pair<String, Double>>, kind: String): S
  *   canBuildFrom(listOf('a', 'b', 'o'), "baobab") -> true
  */
 fun canBuildFrom(chars: List<Char>, word: String): Boolean {
+    if (chars.isEmpty()) return false
+    for (i in 0 until chars.size) chars[i].toLowerCase()
     val letter = chars.toSet()
-    val w = word.toSet()
+    val w = word.toLowerCase().toSet()
     if (letter.containsAll(w)) return true
     return false
 }
